ec3b7411c3335190e891876e6a65ebfc95236ea6
Haiske <160422388+HFutigami@users.noreply.github.com>
2024-07-01 08:13:06 -0300

    Update 2_Varejo.py

--- pages/2_Varejo.py
+++ pages/2_Varejo.py
@@ -709,7 +709,7 @@ else:
         t3r0c1.metric('Total de saídas', '{:,}'.format(sum(df_saidas_varejo_resumido['QUANTIDADE'])).replace(',','.'))
         if len(df_saidas_varejo[df_saidas_varejo['SAÍDA FILA'] >= datetime.today()]) > 0:
             filtro_ontem = ((df_saidas_varejo['SAÍDA FILA'] >= datetime.today()-timedelta(days=1, hours=datetime.today().hour, minutes=datetime.today().minute)) &
-                            (df_saidas_varejo['SAÍDA FILA'] <= datetime.today()-timedelta(hours=datetime.today().hour, minutes=datetime.today().minute)))]
+                            (df_saidas_varejo['SAÍDA FILA'] <= datetime.today()-timedelta(hours=datetime.today().hour, minutes=datetime.today().minute)))
             try:
                 t3r0c2.metric('Saídas do dia', '{:,}'.format(len(df_saidas_varejo[df_saidas_varejo['SAÍDA FILA'] >= datetime.today()])).replace(',','.'),
                             delta='{:.2%}'.format(((len(df_saidas_varejo[df_saidas_varejo['SAÍDA FILA'] >= datetime.today()])) - len(df_saidas_varejo[filtro_ontem])) / len(df_saidas_varejo[df_saidas_varejo['SAÍDA FILA'] >= datetime.today()])))
