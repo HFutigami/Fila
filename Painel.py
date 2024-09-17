87e762a2d607d4e99e6e56d37b4086b94a3b1dfd
Haiske <160422388+HFutigami@users.noreply.github.com>
2024-09-17 08:34:16 -0300

    Update Painel.py

--- Painel.py
+++ Painel.py
@@ -7,29 +7,139 @@ from datetime import datetime
 import io
 import plotly.express as px
 from workalendar.america import Brazil
-from time import sleep
+from datetime import timedelta
+import webbrowser
+import os
 
+### VERIFICA SE FOI ESTABELECIDA UMA CONEXÃO,
+### CASO CONTRÁRIO DIRECIONA O USUÁRIO PARA A TELA INICIAL
 if 'connection' not in st.session_state:
-    with st.form('login', clear_on_submit=True):
-        usuario = st.text_input(label='Usuário')
-        senha = st.text_input(label='Senha', type='password')
-        submitted = st.form_submit_button("Login")
-        if submitted:
-            with st.spinner('Conectando...'):
-                if usuario == st.secrets.credenciais.USER and senha == st.secrets.credenciais.SENHA:
-                    st.session_state['connection'] = 'editor'
-                    st.rerun()
-                else:
-                    st.warning('Usuário ou senha inválidos.', icon="⚠️")
+    st.switch_page('Painel.py')
+
 else:
+
+    ### CONFIGURAÇÕES INICIAS DO STREAMLIT
     st.set_page_config('ESTOQUE • FILA', page_icon='https://i.imgur.com/mOEfCM8.png', layout='wide')
-        
+
     st.image('https://seeklogo.com/images/G/gertec-logo-D1C911377C-seeklogo.com.png?v=637843433630000000', width=200)
     st.header('', divider='gray')
 
     st.sidebar.title('MÓDULOS')
     if st.session_state['connection'] == 'editor':
-        st.sidebar.page_link('pages/4_Movimentações.py', label='MOVIMENTAÇÕES')
+        st.sidebar.page_link('pages/4_Movimentações.py', label='MOVIMENTAÇÕES', disabled=True)
     st.sidebar.page_link('pages/1_Contratos.py', label='CONTRATO')
     st.sidebar.page_link('pages/2_Varejo.py', label='VAREJO')
     st.sidebar.page_link('pages/3_OS Interna.py', label='OS INTERNA')
+
+    ### LINKS ONDE SÃO ARMAZENADOS OS DADOS DO FILA
+    sharepoint_fila_url = 'https://gertecsao.sharepoint.com/sites/PowerBi-Estoque/'
+    sharepoint_os_url = 'https://gertecsao.sharepoint.com/sites/RecebimentoLogstica/'
+    folder_in_sharepoint = '/sites/PowerBi-Estoque/Documentos%20Compartilhados/General/Fila'
+    sharepoint_user = st.secrets.sharepoint.USER
+    sharepoint_password = st.secrets.sharepoint.SENHA
+
+    saldo_fila_url = '/sites/PowerBi-Estoque/Documentos%20Compartilhados/General/Fila/saldo.parquet'
+    varejo_liberado_url = '/sites/PowerBi-Estoque/Documentos%20Compartilhados/General/Fila/Varejo%20Liberado/'
+    sla_contratos_url = '/sites/PowerBi-Estoque/Documentos%20Compartilhados/General/Fila/SlaContratos.csv'
+    abertura_os_url = '/sites/RecebimentoLogstica/Documentos%20Compartilhados/General/Recebimento%20-%20Abertura%20de%20OS.xlsx'
+    prioridade_url = '/sites/PowerBi-Estoque/Documentos%20Compartilhados/General/Fila/Prioridades.xlsx'
+
+    calendario = Brazil()
+
+
+    ### FUNÇÕES
+    def df_sharep(file_url, tipo='parquet', sheet='', site=sharepoint_fila_url):
+        """Gera um DataFrame a partir de um diretório do SharePoint."""
+        auth = AuthenticationContext(site)
+        auth.acquire_token_for_user(sharepoint_user, sharepoint_password)
+        ctx = ClientContext(saldo_fila_url, auth)
+        web = ctx.web
+        ctx.execute_query()
+
+        file_response = File.open_binary(ctx, file_url)
+        bytes_file_obj = io.BytesIO()
+        bytes_file_obj.write(file_response.content)
+        bytes_file_obj.seek(0)
+        if tipo == 'parquet':
+            return pd.read_parquet(bytes_file_obj)
+        elif tipo == 'csv':
+            return pd.read_csv(bytes_file_obj, sep=";")
+        elif tipo == 'excel':
+            if sheet != '':
+                return pd.read_excel(bytes_file_obj, sheet, dtype='str')
+            else:
+                return pd.read_excel(bytes_file_obj, dtype='str')
+
+
+    def create_df_historico_movimentações():
+        # SLA Contratos
+        sla_contratos = df_sharep(sla_contratos_url, tipo="csv")
+        sla_contratos.rename(columns={'SLA': 'PRAZO'}, inplace=True)
+        sla_contratos['FLUXO'] = 'CONTRATO'
+        sla_contratos.set_index(['CLIENTE', 'FLUXO'], inplace=True)
+
+        # Saldo geral
+        historico_fila = df_sharep(saldo_fila_url)
+
+        historico_fila['FLUXO'] = historico_fila['FLUXO'].str.replace('000001', 'CONTRATO')
+        historico_fila['FLUXO'] = historico_fila['FLUXO'].str.replace('000002', 'VAREJO')
+        historico_fila['FLUXO'] = historico_fila['FLUXO'].str.replace('000003', 'VAREJO')
+        historico_fila['FLUXO'] = historico_fila['FLUXO'].str.replace('000004', 'OS INTERNA')
+
+        historico_fila['GARANTIA'] = historico_fila['GARANTIA'].str.upper()
+        historico_fila['CLIENTE'] = historico_fila['CLIENTE'].str.upper()
+
+        historico_fila = historico_fila[historico_fila['ENTRADA GERFLOOR'] != 'Nenhum registro encontrado']
+        historico_fila['ENTRADA GERFLOOR'] = pd.to_datetime(
+            historico_fila.loc[historico_fila['ENTRADA GERFLOOR'] != 'Nenhum registro encontrado', 'ENTRADA GERFLOOR'],
+            format='%d/%m/%Y %I:%M:%S %p')
+
+        historico_fila = historico_fila[['ENDEREÇO',
+                                         'CAIXA',
+                                         'SERIAL',
+                                         'CLIENTE',
+                                         'EQUIPAMENTO',
+                                         'NUM OS',
+                                         'FLUXO',
+                                         'GARANTIA',
+                                         'ENTRADA GERFLOOR',
+                                         'ENTRADA FILA',
+                                         'SAÍDA FILA']]
+
+        historico_fila['ULTIMA DATA'] = historico_fila['SAÍDA FILA']
+        historico_fila.loc[historico_fila['ULTIMA DATA'].isna(), 'ULTIMA DATA'] = datetime.now()
+        historico_fila['AGING TOTAL'] = historico_fila.apply(
+            lambda row: calendario.get_working_days_delta(row['ENTRADA GERFLOOR'], row['ULTIMA DATA']), axis=1) + 1
+        historico_fila['AGING TOTAL'] = historico_fila['AGING TOTAL'].astype('int')
+        historico_fila['AGING FILA'] = historico_fila.apply(
+            lambda row: calendario.get_working_days_delta(row['ENTRADA FILA'], row['ULTIMA DATA']), axis=1) + 1
+        historico_fila['AGING FILA'] = historico_fila['AGING FILA'].astype('int')
+
+        historico_fila = historico_fila.join(sla_contratos, on=['CLIENTE', 'FLUXO'], how='left')
+        historico_fila.loc[historico_fila['PRAZO'].isna(), 'PRAZO'] = 30
+        historico_fila['% DO SLA'] = None
+        historico_fila.loc[~historico_fila['PRAZO'].isna(), '% DO SLA'] = historico_fila['AGING TOTAL'] / \
+                                                                          historico_fila['PRAZO']
+        historico_fila['STATUS'] = None
+        historico_fila.loc[
+            (historico_fila['% DO SLA'] > 0.0) & (historico_fila['% DO SLA'] <= 0.1), 'STATUS'] = "RÁPIDO"
+        historico_fila.loc[(historico_fila['% DO SLA'] > 0.1) & (historico_fila['% DO SLA'] <= 0.3), 'STATUS'] = "MÉDIO"
+        historico_fila.loc[(historico_fila['% DO SLA'] > 0.3) & (historico_fila['% DO SLA'] <= 0.6), 'STATUS'] = "LENTO"
+        historico_fila.loc[
+            (historico_fila['% DO SLA'] > 0.6) & (historico_fila['% DO SLA'] <= 1.0), 'STATUS'] = "CRÍTICO"
+        historico_fila.loc[(historico_fila['% DO SLA'] > 1.0), 'STATUS'] = "SLA ESTOURADO"
+
+        return historico_fila
+
+    
+    if 'historico_fila' not in st.session_state:
+        st.session_state['historico_fila'] = create_df_historico_movimentações()
+        historico_fila = st.session_state['historico_fila']
+    else:
+        historico_fila = st.session_state['historico_fila']
+
+    st.sidebar.header('')
+    st.sidebar.title('AÇÕES')
+
+    st.dataframe(historico_fila[historico_fila['CAIXAS'].isin(df_sharep(prioridade_url, tipo='excel')['CAIXAS'])]) 
+
